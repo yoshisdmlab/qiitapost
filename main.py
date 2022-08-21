75b9e3769fa597ec9b22c85d70a84d130068a75c
yoshisdmlab <yoshihiro.jimbo@sdm-lab.jp>
2022-08-21 18:00:50 +0900

    code: first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import datetime
+
+BASE_URL = "https://qiita.com/api/v2/items"
+TOKEN = "c5761b5f5e6b888304d4f5bfdd50dbb473f2fff8"
+CONF_DIR="article"
+
+def submit_article(path):
+    with open(f"{path}/config.json") as f:
+        conf = f.read()
+    with open(f"{path}/item.md") as f:
+        body = f.read()
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+    item = json.loads(conf)
+    item["body"] = body
+
+    if item["id"] == "":
+        res = requests.post(BASE_URL, headers=headers, json=item)
+        with open(f"{path}/config.json", "w") as f:
+            item["id"] = res.json()["id"]
+            item["body"] = ""
+            f.write(json.dumps(item))
+        return res
+
+    else:
+        now = datetime.datetime.now()
+        item["title"] += now.strftime("【%Y/%m/%d %H時更新】")
+        item_id = item["id"]
+        res = requests.patch(BASE_URL + f"/{item_id}", headers=headers, json=item)
+        return res
+
+
+if __name__ == "__main__":
+    res = submit_article(CONF_DIR).json()
+    print(res["title"], res["url"])
